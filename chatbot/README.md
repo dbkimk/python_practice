ee7fdc4aafc39c1c891dc9e1026f740e1a9cc451
dbkimkiki <kdb4316@gmail.com>
2019-12-20 16:20:12 +0900

    implement chatbot

---
+++ chatbot/README.md
@@ -0,0 +1,35 @@
+### Flask, Telegram Chatbot, 등 (12/20)
+
+
+
+
+
+## ngrok
+
+![image-20191220144033775](Untitled.assets/image-20191220144033775.png)
+
+cmd.exe에서
+
+`ngrok.exe http 5000`
+
+치면 생성됨.
+
+
+
+![image-20191220144345181](Untitled.assets/image-20191220144345181.png)
+
+https://0fb10145.ngrok.io
+
+이라는 주소가 생성됨
+
+https://core.telegram.org/bots/api#getupdates
+
+
+
+https://api.telegram.org/bot995019804:AAFcvvlVXxf82UZCth4ga6F8Sqy2zfNPy8Q/setwebhook?url=https://0fb10145.ngrok.io
+
+
+
+getupdates
+
+setwebhook
--- chatbot/app.py
+++ chatbot/app.py
@@ -1,8 +1,9 @@
 # import telegram
 
-from flask import Flask
+from flask import Flask,request
 import requests
 from decouple import config
+import random
 
 
 app = Flask(__name__)
@@ -10,7 +11,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'hello world!'
-
 
 api_url = 'https://api.telegram.org'
 token = config('TOKEN')
@@ -23,5 +23,28 @@ def send(text='hello'):
     res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return 'ok!'
 
+
+@app.route('/chatbot',methods=['POST'])
+def chatbot():
+    from_telegram = request.get_json()
+    chat_id = from_telegram.get('message').get('from').get('id')
+    text = from_telegram.get('message').get('text')
+    res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+    
+    # status code 200 -> ok 잘 접수했다
+
+    if text=='메뉴':
+        menus = ['양자강','20층','김밥카페']
+        lunch = random.choice(menus)
+        response = lunch
+    elif text=='로또':
+        lotto = random.sample(range(1,46),6)
+        lotto = sorted(lotto)
+        response = f'추천 로또 번호는 {lotto}'
+    else:
+        response = f'너는 {text}라고 보냈는데, 너가 할 줄 아는 건 메뉴야'
+    
+    res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={response}')
+    return 'ok',200
 
 app.run(debug=True)
